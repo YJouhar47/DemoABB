c7dca8203fdc8be3b7c757d0b694ce51ba9082e3
youssef <youssef@inuits.eu>
2023-08-01 12:02:53 +0200

    MU-Authorization added to project

--- config/resources/repository.lisp
+++ config/resources/repository.lisp
@@ -1,5 +1,5 @@
 (in-package :mu-cl-resources)
 
 (add-prefix "dcterms" "http://purl.org/dc/terms/")
-(add-prefix "schema" "http://schema.org")
+(add-prefix "schema" "http://schema.org/")
 (add-prefix "ext" "http://mu.semte.ch/vocabularies/ext/")
